b31f502f4442eb6df10207a18967200d746c90d6
~m6vumHTyE7ZRUht2gBNK~ <163586801+rXZD5cFumBJnx56wcclz@users.noreply.github.com>
2026-09-14 15:53:46 +0300

    struct order_creator added

--- src/gw.rs
+++ src/gw.rs
@@ -1,10 +1,9 @@
+use bc_trade_state::prelude::*;
 use bc_utils_lg::structs::settings::{SETTINGS_ORDER_CREATOR, SETTINGS_TRADE};
-use bc_utils_lg::structs::trade::OrderWrap;
 use bc_utils_lg::{
     structs::{
         settings::{SETTINGS_ORDER_CREATORS, SETTINGS_TRIGGER_OUT_OF_STORAGE},
         signals::Signal,
-        trade::{Order, Trigger},
     },
     types::maps::MAP,
 };
@@ -22,88 +21,95 @@ fn create_trigger(
     })
 }
 
-pub fn create_order(
-    symbol: &str,
-    signals: &MAP<&str, Signal>,
-    ind: &MAP<&str, f64>,
-    utils: &MAP<&str, f64>,
-    s_order_creator: &SETTINGS_ORDER_CREATOR,
-    s_trade: &SETTINGS_TRADE,
-) -> OrderWrap {
-    let signal = signals[s_order_creator.used_signal.as_str()];
-    let price = ind
-        .get(
-            s_order_creator
-                .used_ind
-                .as_ref()
-                .unwrap_or(&"".to_string())
-                .as_str(),
-        )
-        .copied();
-    let qty = utils[s_order_creator.used_util_state.as_str()];
-    let position_idx = if signal.signal == s_trade.signal_long {
-        1
-    } else {
-        2
-    };
-    OrderWrap {
-        order: Order::new(
-            symbol.to_string(),
-            if signal.signal == s_trade.signal_long {
-                "buy".to_string()
-            } else if signal.signal == s_trade.signal_short {
-                "sell".to_string()
-            } else {
-                "hold".to_string()
-            },
-            qty,
-            qty * if s_order_creator.type_ == "market" {
-                s_trade.commission_market
-            } else {
-                s_trade.commission_limit
-            },
-            s_order_creator.leverage,
-            price,
-            s_order_creator.type_.clone(),
-            s_order_creator.is_reduce,
-            s_order_creator.type_price_cross.to_string(),
-            // Uuid::new_v4().to_string(),
-            String::default(),
-            position_idx,
-            true,
-        ),
-        is_trigger: s_order_creator.include_in_storage,
-        trigger: if s_order_creator.include_in_storage {
-            create_trigger(s_order_creator.trigger.as_ref().unwrap(), ind, utils)
-        } else {
-            None
-        },
-    }
-}
+#[derive(Default, Debug)]
+pub struct OrderCreator;
 
-pub fn series<'a>(
-    s: &'a SETTINGS_ORDER_CREATORS,
-    s_trade: &SETTINGS_TRADE,
-    symbol: &str,
-    signals: &MAP<&str, Signal>,
-    indications: &MAP<&str, f64>,
-    res_utils_state: &MAP<&str, f64>,
-) -> MAP<&'a str, OrderWrap> {
-    s.iter()
-        .map(|(k, setting)| {
-            (
-                k.as_str(),
-                create_order(
-                    symbol,
-                    signals,
-                    indications,
-                    res_utils_state,
-                    setting,
-                    s_trade,
-                ),
+impl OrderCreator {
+    pub fn create_order(
+        &self,
+        symbol: &str,
+        signals: &MAP<&str, Signal>,
+        ind: &MAP<&str, f64>,
+        utils: &MAP<&str, f64>,
+        s_order_creator: &SETTINGS_ORDER_CREATOR,
+        s_trade: &SETTINGS_TRADE,
+    ) -> OrderWrap {
+        let signal = signals[s_order_creator.used_signal.as_str()];
+        let price = ind
+            .get(
+                s_order_creator
+                    .used_ind
+                    .as_ref()
+                    .unwrap_or(&"".to_string())
+                    .as_str(),
             )
-        })
-        .collect()
+            .copied();
+        let qty = utils[s_order_creator.used_util_state.as_str()];
+        let position_idx = if signal.signal == s_trade.signal_long {
+            1
+        } else {
+            2
+        };
+        OrderWrap {
+            order: Order::new(
+                symbol.to_string(),
+                if signal.signal == s_trade.signal_long {
+                    "buy".to_string()
+                } else if signal.signal == s_trade.signal_short {
+                    "sell".to_string()
+                } else {
+                    "hold".to_string()
+                },
+                qty,
+                qty * if s_order_creator.type_ == "market" {
+                    s_trade.commission_market
+                } else {
+                    s_trade.commission_limit
+                },
+                s_order_creator.leverage,
+                price,
+                s_order_creator.type_.clone(),
+                s_order_creator.is_reduce,
+                s_order_creator.type_price_cross.to_string(),
+                // Uuid::new_v4().to_string(),
+                String::default(),
+                position_idx,
+                true,
+            ),
+            is_trigger: s_order_creator.include_in_storage,
+            trigger: if s_order_creator.include_in_storage {
+                create_trigger(s_order_creator.trigger.as_ref().unwrap(), ind, utils)
+            } else {
+                None
+            },
+        }
+    }
+
+    pub fn series<'a>(
+        &self,
+        s: &'a SETTINGS_ORDER_CREATORS,
+        s_trade: &SETTINGS_TRADE,
+        symbol: &str,
+        signals: &MAP<&str, Signal>,
+        indications: &MAP<&str, f64>,
+        res_utils_state: &MAP<&str, f64>,
+    ) -> MAP<&'a str, OrderWrap> {
+        s.iter()
+            .map(|(k, setting)| {
+                (
+                    k.as_str(),
+                    self.create_order(
+                        symbol,
+                        signals,
+                        indications,
+                        res_utils_state,
+                        setting,
+                        s_trade,
+                    ),
+                )
+            })
+            .collect()
+    }
 }
 
 #[cfg(test)]
@@ -114,7 +120,7 @@ mod tests {
     #[test]
     fn series_res_1() {
         assert_eq_pr!(
-            &series(
+            &OrderCreator.series(
                 &ORDER_CREATORS,
                 &TRADE,
                 "",
